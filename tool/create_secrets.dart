d7446b39d3aae3f05e9dcd0bfa2c43abfd2f3603
konstantinullrich <konstantin.ullrich@consurance.de>
2020-10-23 15:28:06 +0200

    Add create Secret tool

---
+++ tool/create_secrets.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+
+const secretsProdPath = 'tool/.secrets-prod.json';
+const secretsTestPath = 'tool/.secrets-test.json';
+
+Future<void> main() async {
+  final inputPath = FileSystemEntity.typeSync(secretsProdPath) !=
+      FileSystemEntityType.notFound
+      ? secretsProdPath
+      : secretsTestPath;
+
+  const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  final _rnd = Random();
+
+  String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+      length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+
+  await File(inputPath).writeAsString(
+      jsonEncode(
+          {
+            'salt': getRandomString(32),
+            'key': getRandomString(32),
+            'walletSalt': getRandomString(8),
+            'shortKey': getRandomString(24),
+            'change_now_api_key': ''
+          }
+      )
+  );
+}
