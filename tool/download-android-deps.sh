85e05b07a592a168e6c82590d7e7a98e8d4ac12c
Jason Rhinelander <jason@imaginary.ca>
2021-01-11 00:12:02 -0400

    Add android deps download tool

---
+++ tool/download-android-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ] || [[ "$1" != http* ]]; then
+    echo "Usage: $0 URL -- download and extract an oxen-core android-deps package (typically from https://oxen.rocks)" >&2
+    exit 1
+fi
+
+if ! [ -d oxen_coin/ios/External/android/oxen ]; then
+    echo "This script needs to be invoked from the oxen-wallet top-level project directory" >&2
+    exit 1
+fi
+
+curl -sS "$1" | tar --strip-components=1 -C oxen_coin/ios/External/android/oxen/ -xJv
