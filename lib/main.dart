ce5de90b2c8bae110f07cfaad3e75331b424570c
Jason Rhinelander <jason@imaginary.ca>
2021-01-29 13:06:28 -0400

    Add exception wrapper around main initialization
    If something goes wrong here this prints the exception message rather
    than just leaving the user with a blank screen.

--- lib/main.dart
+++ lib/main.dart
@@ -37,89 +37,105 @@ import 'package:oxen_wallet/src/domain/common/language.dart';
 import 'package:oxen_wallet/src/stores/seed_language/seed_language_store.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
 
-  final appDir = await getApplicationDocumentsDirectory();
-  Hive.init(appDir.path);
-  Hive.registerAdapter(ContactAdapter());
-  Hive.registerAdapter(NodeAdapter());
-  Hive.registerAdapter(TransactionDescriptionAdapter());
-  Hive.registerAdapter(WalletInfoAdapter());
-  Hive.registerAdapter(WalletTypeAdapter());
+    final appDir = await getApplicationDocumentsDirectory();
+    Hive.init(appDir.path);
+    Hive.registerAdapter(ContactAdapter());
+    Hive.registerAdapter(NodeAdapter());
+    Hive.registerAdapter(TransactionDescriptionAdapter());
+    Hive.registerAdapter(WalletInfoAdapter());
+    Hive.registerAdapter(WalletTypeAdapter());
 
-  final secureStorage = FlutterSecureStorage();
-  final transactionDescriptionsBoxKey = await getEncryptionKey(
-      secureStorage: secureStorage,
-      forKey: 'transactionDescriptionsBoxKey'); // FIXME: Unnamed constant
+    final secureStorage = FlutterSecureStorage();
+    final transactionDescriptionsBoxKey = await getEncryptionKey(
+        secureStorage: secureStorage,
+        forKey: 'transactionDescriptionsBoxKey'); // FIXME: Unnamed constant
 
-  final contacts = await Hive.openBox<Contact>(Contact.boxName);
-  final nodes = await Hive.openBox<Node>(Node.boxName);
-  final transactionDescriptions = await Hive.openBox<TransactionDescription>(
-      TransactionDescription.boxName,
-      encryptionKey: transactionDescriptionsBoxKey);
-  final walletInfoSource = await Hive.openBox<WalletInfo>(WalletInfo.boxName);
+    final contacts = await Hive.openBox<Contact>(Contact.boxName);
+    final nodes = await Hive.openBox<Node>(Node.boxName);
+    final transactionDescriptions = await Hive.openBox<TransactionDescription>(
+        TransactionDescription.boxName,
+        encryptionKey: transactionDescriptionsBoxKey);
+    final walletInfoSource = await Hive.openBox<WalletInfo>(WalletInfo.boxName);
 
-  final sharedPreferences = await SharedPreferences.getInstance();
-  final walletService = WalletService();
-  final walletListService = WalletListService(
-      secureStorage: secureStorage,
-      walletInfoSource: walletInfoSource,
-      walletService: walletService,
-      sharedPreferences: sharedPreferences);
-  final userService = UserService(
-      sharedPreferences: sharedPreferences, secureStorage: secureStorage);
-  final authenticationStore = AuthenticationStore(userService: userService);
+    final sharedPreferences = await SharedPreferences.getInstance();
+    final walletService = WalletService();
+    final walletListService = WalletListService(
+        secureStorage: secureStorage,
+        walletInfoSource: walletInfoSource,
+        walletService: walletService,
+        sharedPreferences: sharedPreferences);
+    final userService = UserService(
+        sharedPreferences: sharedPreferences, secureStorage: secureStorage);
+    final authenticationStore = AuthenticationStore(userService: userService);
 
-  await initialSetup(
-      sharedPreferences: sharedPreferences,
-      walletListService: walletListService,
-      nodes: nodes,
-      authStore: authenticationStore,
-      initialMigrationVersion: 2);
+    await initialSetup(
+        sharedPreferences: sharedPreferences,
+        walletListService: walletListService,
+        nodes: nodes,
+        authStore: authenticationStore,
+        initialMigrationVersion: 2);
 
-  final settingsStore = await SettingsStoreBase.load(
-      nodes: nodes,
-      sharedPreferences: sharedPreferences,
-      initialFiatCurrency: FiatCurrency.usd,
-      initialTransactionPriority: OxenTransactionPriority.blink,
-      initialBalanceDisplayMode: BalanceDisplayMode.availableBalance);
-  final priceStore = PriceStore();
-  final walletStore =
-      WalletStore(walletService: walletService, settingsStore: settingsStore);
-  final syncStore = SyncStore(walletService: walletService);
-  final balanceStore = BalanceStore(
-      walletService: walletService,
-      settingsStore: settingsStore,
-      priceStore: priceStore);
-  final loginStore = LoginStore(
-      sharedPreferences: sharedPreferences, walletsService: walletListService);
-  final seedLanguageStore = SeedLanguageStore();
+    final settingsStore = await SettingsStoreBase.load(
+        nodes: nodes,
+        sharedPreferences: sharedPreferences,
+        initialFiatCurrency: FiatCurrency.usd,
+        initialTransactionPriority: OxenTransactionPriority.blink,
+        initialBalanceDisplayMode: BalanceDisplayMode.availableBalance);
+    final priceStore = PriceStore();
+    final walletStore =
+        WalletStore(walletService: walletService, settingsStore: settingsStore);
+    final syncStore = SyncStore(walletService: walletService);
+    final balanceStore = BalanceStore(
+        walletService: walletService,
+        settingsStore: settingsStore,
+        priceStore: priceStore);
+    final loginStore = LoginStore(
+        sharedPreferences: sharedPreferences, walletsService: walletListService);
+    final seedLanguageStore = SeedLanguageStore();
 
-  setReactions(
-      settingsStore: settingsStore,
-      priceStore: priceStore,
-      syncStore: syncStore,
-      walletStore: walletStore,
-      walletService: walletService,
-      authenticationStore: authenticationStore,
-      loginStore: loginStore);
+    setReactions(
+        settingsStore: settingsStore,
+        priceStore: priceStore,
+        syncStore: syncStore,
+        walletStore: walletStore,
+        walletService: walletService,
+        authenticationStore: authenticationStore,
+        loginStore: loginStore);
 
-  runApp(MultiProvider(providers: [
-    Provider(create: (_) => sharedPreferences),
-    Provider(create: (_) => walletService),
-    Provider(create: (_) => walletListService),
-    Provider(create: (_) => userService),
-    Provider(create: (_) => settingsStore),
-    Provider(create: (_) => priceStore),
-    Provider(create: (_) => walletStore),
-    Provider(create: (_) => syncStore),
-    Provider(create: (_) => balanceStore),
-    Provider(create: (_) => authenticationStore),
-    Provider(create: (_) => contacts),
-    Provider(create: (_) => nodes),
-    Provider(create: (_) => transactionDescriptions),
-    Provider(create: (_) => seedLanguageStore)
-  ], child: OxenWalletApp()));
+    runApp(MultiProvider(providers: [
+      Provider(create: (_) => sharedPreferences),
+      Provider(create: (_) => walletService),
+      Provider(create: (_) => walletListService),
+      Provider(create: (_) => userService),
+      Provider(create: (_) => settingsStore),
+      Provider(create: (_) => priceStore),
+      Provider(create: (_) => walletStore),
+      Provider(create: (_) => syncStore),
+      Provider(create: (_) => balanceStore),
+      Provider(create: (_) => authenticationStore),
+      Provider(create: (_) => contacts),
+      Provider(create: (_) => nodes),
+      Provider(create: (_) => transactionDescriptions),
+      Provider(create: (_) => seedLanguageStore)
+    ], child: OxenWalletApp()));
+  } catch (e) {
+    runApp(MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+        body: Container(
+          margin:
+            EdgeInsets.only(top: 50, left: 20, right: 20, bottom: 20),
+          child: Text(
+            'Error:\n${e.toString()}',
+            style: TextStyle(fontSize: 22),
+          )
+        )
+      )
+    ));
+  }
 }
 
 Future<void> initialSetup(
